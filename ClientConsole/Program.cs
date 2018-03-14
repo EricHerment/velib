f1754ef91436847727fd114cd4fe9a7aa88c064e
Eric HERMENT <ericherment@outlook.com>
2018-03-14 14:52:22 +0100

    client console

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string commands = "Type the name of your station\n" +
+                              "Type 'stations' for a list of the stations availables\n" +
+                              "Type 'help' for help\n" +
+                              "Type 'exit' to exit\n";
+                              
+            
+            Console.WriteLine("Welcome !");
+            Console.WriteLine(commands);
+
+            VelibClient.Service1Client velibClient = new VelibClient.Service1Client();
+
+            Boolean serve = true;
+            while (serve)
+            {
+                
+                string input = Console.ReadLine();
+
+                switch(input)
+                {
+                    case "help":
+                        Console.WriteLine(commands);
+                        break;
+                    case "exit":
+                        serve = false;
+                        break;
+                    case "stations":
+                        break;
+                    default:
+                        Console.WriteLine(velibClient.GetData(input));
+                        break;
+                }
+                
+            }
+            
+            
+        }
+    }
+}
--- velib/Service1.cs
+++ velib/Service1.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
@@ -10,22 +13,54 @@ namespace velib
     // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        static string token = "b0e7b9c0530c7797ba8f5dfdff2ea74e47fcf958";
+
+
+        public string GetData(string stationName)
         {
-            return string.Format("You entered: {0}", value);
+            WebRequest request = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=" + token);
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string serverResponse = reader.ReadToEnd();
+
+            string research = stationName;
+            string responseFromServer = serverResponse;
+            JArray j = JArray.Parse(responseFromServer);
+            int number = 0;
+            string station = null;
+
+            foreach (JObject item in j)
+            {
+                string name = (string) item.SelectToken("name");
+
+                if (name.ToLower().Contains(research.ToLower()))
+                {
+                    number = (int) item.SelectToken("available_bikes");
+                    station = name;
+                    break;
+                }
+            }
+
+            StringBuilder output = new StringBuilder();
+
+            if (station != null)
+            {
+                output.AppendLine("Station :" + station);
+                output.AppendLine("Nombre de vélos disponibles :" + number);
+            }
+            else
+            {
+                output.AppendLine("Invalid name");
+                output.AppendLine("Type 'stations' for a list of the stations available");
+            }
+               
+
+            return output.ToString();
+
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+        
     }
 }
